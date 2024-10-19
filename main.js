de94075cbbe3f43b7f2000c3ef770e4a69de01cc
Miodrag <9781533+modularscript@users.noreply.github.com>
2024-10-19 11:51:32 +0200

    Grid media queries prototype

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-import "./style.scss"
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
+import './style.scss';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
 
-
-
-
-// import { setupCounter } from './counter.js'
+// import { setupCounter } from './_script/counter.js';
 
 // document.querySelector('#app').innerHTML = `
-//   <div>
+//     <div>
 //     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//       <img src="" class="logo" alt="Vite logo" />
 //     </a>
 //     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//        <img src="" class="logo vanilla" alt="JavaScript logo" />
 //     </a>
 //     <h1>Hello Vite!</h1>
 //     <div class="card">
@@ -23,6 +20,6 @@ import "./style.scss"
 //       Click on the Vite logo to learn more
 //     </p>
 //   </div>
-// `
+// `;
 
-// setupCounter(document.querySelector('#counter'))
+// setupCounter(document.querySelector('#counter'));
